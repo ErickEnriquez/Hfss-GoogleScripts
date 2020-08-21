b8cebaa438c580058f19346ad8da9b10b217d2c0
Erick Enriquez <erick@hubbardswim.com>
2020-08-21 15:10:48 -0700

    [trying to add html sheet
    trying to create a html sheet to set text, need to find out what is going on with text area.value

--- SendSMS/send_out_custom_sms.js
+++ SendSMS/send_out_custom_sms.js
@@ -3,67 +3,79 @@ const SID = "AC9f2f8a075203b765ee7724fbf1c8bb17";
 const token = "d53462cfa67afe893f96c6a7bad52cc7";
 const companyPhone = "6026712206";
 const turtlesImage =
-    "https://hfss-website.s3-us-west-2.amazonaws.com/turtles.png";
+  "https://hfss-website.s3-us-west-2.amazonaws.com/turtles.png";
 ///////////////////////////////////////////////////////////////////////////////////////
 
-//==================================================================================================================================
-//main driver for the entire script
-function main() {
-  let message = showInputPrompt()
-
-
+//======================================================================================================================================================
+//Creates the Custom Menu that will display show sidebar
+function onOpen() {
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+    .createMenu("Custom Menu")
+    .addItem("Show sidebar", "showSidebar")
+    .addToUi();
 }
-
-//==================================================================================================================================
-function showInputPrompt() {
-    let ui = SpreadsheetApp.getUi();
-    let response = ui.prompt('Enter your message here', 'To make personalized message include ${name} where you want to add persons name ie \"Hello ${name}\"', ui.ButtonSet.YES_NO)
-    return response
+//======================================================================================================================================================
+//calls Page.html to render the html as a sidebar
+function showSidebar() {
+  var html = HtmlService.createHtmlOutputFromFile("Page")
+    .setTitle("Helper")
+    .setWidth(800);
+  SpreadsheetApp.getUi().showSidebar(html);
 }
+//======================================================================================================================================================
 
-//==================================================================================================================================
-
-//==================================================================================================================================
+// Sets the value of A1 cell to value entered in the input field in the side bar!
+function enterName(name) {
+  var ss = SpreadsheetApp.getActive();
+  var sheet = ss.getActiveSheet();
+  sheet.getRange("A1").setValue(name);
+}
+//======================================================================================================================================================
 
 function sendSms(to, body) {
-    var messages_url =
-        "https://api.twilio.com/2010-04-01/Accounts/" + SID + "/Messages.json";
+  var messages_url =
+    "https://api.twilio.com/2010-04-01/Accounts/" + SID + "/Messages.json";
 
-    var payload = {
-        To: to,
-        Body: body,
-        From: companyPhone,
-    };
+  var payload = {
+    To: to,
+    Body: body,
+    From: companyPhone,
+  };
 
-    var options = {
-        method: "post",
-        payload: payload,
-    };
+  var options = {
+    method: "post",
+    payload: payload,
+  };
 
-    options.headers = {
-        Authorization: "Basic " + Utilities.base64Encode(SID + ":" + token),
-    };
+  options.headers = {
+    Authorization: "Basic " + Utilities.base64Encode(SID + ":" + token),
+  };
 
-    UrlFetchApp.fetch(messages_url, options);
+  UrlFetchApp.fetch(messages_url, options);
 }
 
-//==================================================================================================================================
+//======================================================================================================================================================
 
 //takes a the formatted data and passes it to send SMS function to send text out
 function sendOutTexts() {
-    let spreadsheet = SpreadsheetApp.getActive();
-    let data = spreadsheet.getDataRange().getValues();
-    let status = "";
-    for (i = 1; i < data.length; i++) {
-        let row = data[i];    
+  let spreadsheet = SpreadsheetApp.getActive();
+  let data = spreadsheet.getDataRange().getValues();
+  let status = "";
+  for (i = 1; i < data.length; i++) {
+    let row = data[i];
 
-        try {
-            response_data = sendSms(String(row[2]), messageText);
-            status = "sent";
-        } catch (err) {
-            Logger.log(err);
-            status = "error";
-        }
-        spreadsheet.getRange("C" + (Number(i) + 1)).setValue(status);
+    try {
+      response_data = sendSms(String(row[2]), messageText);
+      status = "sent";
+    } catch (err) {
+      Logger.log(err);
+      status = "error";
     }
+    spreadsheet.getRange("C" + (Number(i) + 1)).setValue(status);
+  }
+}
+//======================================================================================================================================================
+//Simple utility function to print out an an output
+function printObject(output){
+    SpreadsheetApp.getUi().alert('Hello, world' + output + ' IS WHAT IT SHOULD SAY');
 }
