ceeb3f7236d1efc0155400945358537e4fb805fc
Erick Enriquez <erick@hubbardswim.com>
2020-09-02 13:36:11 -0700

    should work, needs work on replace looks like

--- SendSMS/send_out_custom_sms.js
+++ SendSMS/send_out_custom_sms.js
@@ -4,7 +4,8 @@ const token = "d53462cfa67afe893f96c6a7bad52cc7";
 const companyPhone = "6026712206";
 const turtlesImage =
   "https://hfss-website.s3-us-west-2.amazonaws.com/turtles.png";
-const MASTER_SHEET = "PASTE NAMES HERE"
+const MASTER_SHEET = "PASTE NAMES HERE";
+const OUTPUT_SHEET = "RESULTS";
 ///////////////////////////////////////////////////////////////////////////////////////
 
 //======================================================================================================================================================
@@ -52,38 +53,90 @@ function sendSms(to, body) {
 //======================================================================================================================================================
 
 //takes a the formatted data and passes it to send SMS function to send text out
-function sendOutTexts() {
+function sendOutTexts(res) {
   let spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.setActiveSheet(spreadsheet.getSheetByName(MASTER_SHEET), true);
   let data = spreadsheet.getDataRange().getValues();
+  const phoneColumnIndex = getPhoneColumnIndex(res.phoneNumberColumn)
   let status = "";
-  for (i = 1; i < data.length; i++) {
-    let row = data[i];
+  let output = [];
+  let temp = {};
+  /*SpreadsheetApp.getUi().alert(
+    "HEADERS CHECKED " +
+      res.headerRows +
+      "\nMESSAGE TEXT " +
+      res.message +
+      "\nPHONE NUM COL " +
+      res.phoneNumberColumn
+  );*/
+  for (let i in data) {
+    let text = replaceSmartTags(res.message, data[i]);
+    SpreadsheetApp.getUi().alert(text)
 
     try {
-      response_data = sendSms(String(row[2]), messageText);
-      status = "sent";
+      //responseData = sendSms(res.phoneNumberColumn, text)
+      status = "Sent";
     } catch (err) {
       Logger.log(err);
-      status = "error";
+      status = "Error";
     }
-    spreadsheet.getRange("C" + (Number(i) + 1)).setValue(status);
+    temp.textStatus = status;
+    temp.phoneNumber = data[phoneColumnIndex]; 
+    output.push(temp);
   }
+
+  writeResults(output);
 }
 //======================================================================================================================================================
 //Simple utility function to print out an an output
-function printObject(output){
-    SpreadsheetApp.getUi().alert(output);
+function printObject(output) {
+  SpreadsheetApp.getUi().alert(output);
 }
 
 //======================================================================================================================================================
-//  GRAB HEADER ROW FROM THE TOP
-function getHeaderRow(){
-  let spreadsheet = SpreadsheetApp.getActive();
-  spreadsheet.setActiveSheet(
-    spreadsheet.getSheetByName(MASTER_SHEET),
-    true
-);
-let data = spreadsheet.getDataRange().getValues();
-return data[0] //return the header row
+//function to replace all of the smart tags with their string literals
+function replaceSmartTags(response, data) {
+  //let smartTags = /\{[a-e]\}/gi; // regex to grab all tags of the form {a} - {e} case insensitive
+  //const regex1 = new RegExp("/{[a-e]}/", "ig");
+  let text = response.message.replace(/\{a\}/gi, data[0]);
+  text = text.replace(/\{b\}/gi, data[1]);
+  text = text.replace(/\{c\}/gi, data[2]);
+  text = text.replace(/\{d\}/gi, data[3]);
+  text = text.replace(/\{e\}/gi, data[4]);
+  return text;
+}
 
+//======================================================================================================================================================
+//This function switches to results sheet and gives the results of the text
+function writeResult(output) {
+  let spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.setActiveSheet(spreadsheet.getSheetByName(OUTPUT_SHEET), true);
+  spreadsheet.getDataRange().clearContent(); // clear out old content from spreadsheet
+  const headerRow = "A1:B1";
+  spreadsheet.getRange(headerRow).setValues([["Phone Number", "Text Status"]]);
+  rowIndex = 2;
+  for (let i in output) {
+    let row = "A" + rowIndex + ":B" + rowIndex;
+    spreadsheet
+      .getRange(row)
+      .setValues([[output[i].phoneNumber, output[i].textStatus]]);
+    rowIndex++;
+  }
+}
+
+//======================================================================================================================================================
+//gets the index of the phone number column where input is the letters a-e
+function getPhoneColumnIndex(input) {
+  switch (input) {
+    case 'A':
+      return 0;
+    case 'B':
+      return 1;
+    case 'C':
+      return 2
+    case 'D': 
+      return 3
+    case 'E':
+      return 4
+  }
 }
